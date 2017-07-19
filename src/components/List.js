6fc12fcb2c0e8ffdc87b3542189c31906ecb2eb8
RanjithaGade <ranjithagade@gmail.com>
2017-07-19 15:42:55 -0400

    FirstCommit

---
+++ src/components/List.js
@@ -0,0 +1,61 @@
+import React, { PropTypes } from 'react';
+import { Row, Col, Table, Button } from 'react-bootstrap';
+import Item from './Item';
+const getRandomBackground= ()=>{
+  const colors = {
+    0:"yellow",
+    1:"blue",
+    2:"white",
+    3:"green",
+    4:"aqua"
+  };
+  const random = Math.ceil(Math.random()*10);
+  return random>4?colors[random-5]:colors[random];
+}
+export default class List extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      data:[]
+    };
+  }
+  componentWillMount(){
+    this.setState({
+      data: this.props.data
+    });
+  }
+  componentWillReceiveProps(nextProps){
+    this.setState({
+      data: nextProps.data
+    })
+  }
+  render(){
+    return(
+      <Row style={{backgroundColor:getRandomBackground()}}>
+        <Col lg={12} md={12} sm={12}>
+          <label htmlFor="tops" className="header">{this.props.header}</label>
+          <Table id="tops">
+							<tr>
+                {
+                  this.state.data.length?this.state.data.map((d,i)=>{
+                    return(
+                      <td key={i} lg={2}>
+                        <Item item={d} buttonClick={this.props.onClick} buttonName={this.props.button} />
+        							</td>
+                    )
+                  }):<div className="empty">Please add some recommendations</div>
+                }
+              </tr>
+						</Table>
+        </Col>
+      </Row>
+    );
+  }
+}
+
+List.propTypes = {
+  header: PropTypes.string.isRequired,
+  data: PropTypes.array,
+  button: PropTypes.string,
+  onClick: PropTypes.func
+}
---
+++ src/reducers/reducer.js
@@ -0,0 +1,36 @@
+import {
+    GET_DATA,
+    ADD_ITEM,
+    REMOVE_ITEM
+} from '../actions/actionTypes';
+const initialState = {};
+
+export default function reducer(state = initialState, action) {
+    let newState = Object.assign([], state);
+    switch (action.type) {
+        case GET_DATA:
+            return action.payload;
+            break;
+        case ADD_ITEM:
+            if (newState.mylist.find((item) => {
+                    return item.id === action.item.id
+                })) {
+                return newState;
+            } else {
+                return Object.assign({}, state, {
+                    mylist: [...state.mylist, action.item]
+                })
+            }
+            break;
+        case REMOVE_ITEM:
+            return Object.assign({}, newState, {
+                mylist: Object.assign([], newState.mylist.filter(item => {
+                    return item.id != action.item.id
+                }))
+            })
+            break;
+        default:
+            return state;
+            break;
+    }
+}
